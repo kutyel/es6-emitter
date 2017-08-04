8e26ac92bef989c5dfbf4ed612b13ebcc65af21f
kutyel <flaviocorpa@gmail.com>
2017-08-04 11:09:52 +0200

    Use `toHaveBeenCalled()` Jest API 🃏👏🏼

--- index.test.js
+++ index.test.js
@@ -4,43 +4,43 @@ const Emitter = require('.')
 
 describe('Emitter', () => {
   let sub
-  const callback = jest.fn()
+  const callback1 = jest.fn()
+  const callback2 = jest.fn()
   const emitter = new Emitter()
 
   test('should trigger nothing', () => {
-    emitter.emit('sum', 1)
-    expect(callback.mock.calls.length).toBe(0)
+    emitter.emit('event', 'foo')
+    expect(callback1).not.toHaveBeenCalled()
   })
 
   test('should trigger 1 callback', () => {
-    sub = emitter.subscribe('sum', callback)
-    emitter.emit('sum', 1)
-    expect(callback.mock.calls.length).toBe(1)
-    expect(callback.mock.calls[0][0]).toBe(1)
+    sub = emitter.subscribe('event', callback1)
+    emitter.emit('event', 'foo')
+    expect(callback1).toHaveBeenCalledTimes(1)
+    expect(callback1).toHaveBeenCalledWith('foo')
   })
 
   test('should trigger 2 callbacks', () => {
-    emitter.subscribe('sum', callback)
-    emitter.emit('sum', 2)
-    expect(callback.mock.calls.length).toBe(3)
-    expect(callback.mock.calls[0][0]).toBe(1)
-    expect(callback.mock.calls[1][0]).toBe(2)
-    expect(callback.mock.calls[2][0]).toBe(2)
+    emitter.subscribe('event', callback2)
+    emitter.emit('event', 'bar', 'baz')
+    expect(callback1).toHaveBeenCalledTimes(2)
+    expect(callback1).toHaveBeenCalledWith('bar', 'baz')
+    expect(callback2).toHaveBeenCalledTimes(1)
+    expect(callback2).toHaveBeenCalledWith('bar', 'baz')
   })
 
   test('should release first callback, and call the second', () => {
     sub()
-    emitter.emit('sum', 3)
-    expect(callback.mock.calls.length).toBe(3)
-    expect(callback.mock.calls[0][0]).toBe(1)
-    expect(callback.mock.calls[1][0]).toBe(2)
-    expect(callback.mock.calls[2][0]).toBe(2)
+    emitter.emit('event', 'meow')
+    expect(callback1).toHaveBeenCalledTimes(2) // same number as before
+    expect(callback2).toHaveBeenCalledTimes(2)
+    expect(callback2).toHaveBeenCalledWith('meow')
   })
 
   test('should return the return values of all the events in the callstack', () => {
-    emitter.subscribe('myEvent', jest.fn().mockReturnValue(1))
-    emitter.subscribe('myEvent', jest.fn().mockReturnValue(2))
-    emitter.subscribe('myEvent', jest.fn().mockReturnValue(true))
+    emitter.subscribe('myEvent', () => 1)
+    emitter.subscribe('myEvent', () => 2)
+    emitter.subscribe('myEvent', () => true)
     const result = emitter.emit('myEvent')
     expect(result).toEqual([1, 2, true])
   })
